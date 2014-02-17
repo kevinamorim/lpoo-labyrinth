0c3634fe5b52d216119b2e0ef3f29d830920ed5e
Kevin Amorim <ptkevin.a@gmail.com>
2014-02-17 18:05:54 +0000

    - Game class. - Labyrinth printing.

--- src/Labyrinth.java
+++ src/Labyrinth.java
@@ -1,4 +1,29 @@
 
 public class Labyrinth {
+	
+	private char tiles[][];
+
+	public Labyrinth(char[][] tiles) {
+		
+		this.tiles = tiles;
+		
+	}
+	
+	public void DrawBoard() {
+		
+		for(char[] line : tiles) {
+			
+			for(char tile : line) {
+				
+				System.out.print(" " + tile + " ");
+				
+			}
+			
+			System.out.println();
+		}
+		
+	}
+	
+	
 
 }
