b64e7bf46cb4d83e88d52f8ccae7784cc4ded5e8
kevinamorim <ptkevin.a@gmail.com>
2014-02-22 16:21:46 +0000

    - Maze generation.

--- src/Game.java
+++ src/Game.java
@@ -6,7 +6,7 @@ public class Game {
 		{ 'x', ' ', 'x', 'x', ' ', 'x', ' ', 'x', ' ', 'x' },
 		{ 'x', ' ', 'x', 'x', ' ', 'x', ' ', 'x', ' ', 'x' },
 		{ 'x', ' ', 'x', 'x', ' ', 'x', ' ', 'x', ' ', 'x' },
-		{ 'x', ' ', ' ', ' ', ' ', ' ', ' ', 'x', ' ', 'S' },
+		{ 'x', ' ', ' ', ' ', ' ', ' ', ' ', 'x', ' ', 'x' },
 		{ 'x', ' ', 'x', 'x', ' ', 'x', ' ', 'x', ' ', 'x' },
 		{ 'x', ' ', 'x', 'x', ' ', 'x', ' ', 'x', ' ', 'x' },
 		{ 'x', ' ', 'x', 'x', ' ', ' ', ' ', ' ', ' ', 'x' },
@@ -15,120 +15,122 @@ public class Game {
 	
 	public static void main(String[] args) {
 		
-		// Creates a new Input type object (receives keyboard inputs)
-		Input in = new Input();
+//		// Creates a new Input type object (receives keyboard inputs)
+//		Input in = new Input();
+//		
+//		// Creates the Labyrinth
+//		Labyrinth lab = new Labyrinth(board, 10);
+//		
+//		// Creates our hero/player
+//		Hero player = new Hero(1, 1);
+//		
+//		// Creates our sword
+//		Sword sword = new Sword(8, 1);
+//		
+//		// Creates our evil Dragon!
+//		Dragon dragon = new Dragon(3, 1);
+//		
+//		// Sets the player's position on the labyrinth (effectively places it's reresenting char)
+//		lab.setPlayer(player);
+//		
+//		// Sets the dragon's position on the labyrinth (effectively places it's reresenting char)
+//		lab.setDragon(dragon);
+//		
+//		// Sets the sword's position on the labyrinth (effectively places it's reresenting char)
+//		lab.setSword(sword);
+//		
+//		// Draws the Labyrinth matrix/board
+//		lab.DrawBoard();
+//		
+//		// Game loop
+//		while(true) {
+//			
+//			// Gets user input.
+//			in.getInput();
+//			
+//			// Handles user input.
+//			for(int i = 0; i < Input.NKEYS; i++) {
+//				
+//				if(in.getKeys()[i]) {
+//					
+//					Input.KEY direction = Input.KEY.values()[i];
+//					switch(direction) {
+//					case UP:
+//						player.MoveUp(lab);
+//						break;
+//					case RIGHT:
+//						player.MoveRight(lab);
+//						break;
+//					case DOWN:
+//						player.MoveDown(lab);
+//						break;
+//					case LEFT:
+//						player.MoveLeft(lab);
+//						break;
+//					default:
+//						break;
+//					}
+//					
+//					break;
+//				}
+//				
+//			}
+//			
+//			// Moves dragon.
+//			dragon.MoveDragon(lab);
+//			
+//			
+//			lab.setPlayer(player);
+//		
+//			lab.setDragon(dragon);
+//			
+//		
+//			// Checks if the player has encountered the Dragon, that terrible beast!
+//			if(lab.foundDragon(player, dragon) && (dragon.isAlive())) {
+//				// If the player is armed, it slains the Dragon
+//				if(player.isArmed()) {
+//					lab.killDragon(dragon);
+//				}
+//				// If the player is unarmed, it gets killed
+//				else {
+//					player.setAlive(false);
+//					break;
+//				}
+//			}
+//			
+//			// Checks if the player has found the sword
+//			// If so, the player's char goes from 'H' (hero) to 'A' (armed hero)
+//			if(lab.foundSword(player)) {
+//				player.setArmed(true);
+//				player.setHeroChar('A');
+//			}
+//			
+//			// Checks if the player is at the Labyrinth exit
+//			if(lab.isAtExit(player)) {
+//				
+//				// If the dragon is slained our hero may sucessfully exit the Labyrinth 
+//				if(!dragon.isAlive())
+//					break;
+//				else
+//					player.MoveBack(lab);
+//			}
+//
+//			
+//			// Only draws sword if dragon has not the sword and if it's not taken by the player.
+//			if(!dragon.hasSword() && !player.isArmed()) lab.setSword(sword);
+//			
+//			lab.DrawBoard();
+//			
+//		}
+//		
+//		lab.setPlayer(player);
+//		
+//		lab.DrawBoard();
+//		
+//		if(player.isAlive()) System.out.print(" WIN ");
+//		else System.out.print(" LOSE ");
 		
-		// Creates the Labyrinth
-		Labyrinth lab = new Labyrinth(board);
-		
-		// Creates our hero/player
-		Hero player = new Hero(1, 1);
-		
-		// Creates our sword
-		Sword sword = new Sword(8, 1);
-		
-		// Creates our evil Dragon!
-		Dragon dragon = new Dragon(3, 1);
-		
-		// Sets the player's position on the labyrinth (effectively places it's reresenting char)
-		lab.setPlayer(player);
-		
-		// Sets the dragon's position on the labyrinth (effectively places it's reresenting char)
-		lab.setDragon(dragon);
-		
-		// Sets the sword's position on the labyrinth (effectively places it's reresenting char)
-		lab.setSword(sword);
-		
-		// Draws the Labyrinth matrix/board
-		lab.DrawBoard();
-		
-		// Game loop
-		while(true) {
-			
-			// Gets user input.
-			in.getInput();
-			
-			// Handles user input.
-			for(int i = 0; i < Input.NKEYS; i++) {
-				
-				if(in.getKeys()[i]) {
-					
-					Input.KEY direction = Input.KEY.values()[i];
-					switch(direction) {
-					case UP:
-						player.MoveUp(lab);
-						break;
-					case RIGHT:
-						player.MoveRight(lab);
-						break;
-					case DOWN:
-						player.MoveDown(lab);
-						break;
-					case LEFT:
-						player.MoveLeft(lab);
-						break;
-					default:
-						break;
-					}
-					
-					break;
-				}
-				
-			}
-			
-			// Moves dragon.
-			dragon.MoveDragon(lab);
-			
-			
-			lab.setPlayer(player);
-		
-			lab.setDragon(dragon);
-			
-		
-			// Checks if the player has encountered the Dragon, that terrible beast!
-			if(lab.foundDragon(player, dragon) && (dragon.isAlive())) {
-				// If the player is armed, it slains the Dragon
-				if(player.isArmed()) {
-					lab.killDragon(dragon);
-				}
-				// If the player is unarmed, it gets killed
-				else {
-					player.setAlive(false);
-					break;
-				}
-			}
-			
-			// Checks if the player has found the sword
-			// If so, the player's char goes from 'H' (hero) to 'A' (armed hero)
-			if(lab.foundSword(player)) {
-				player.setArmed(true);
-				player.setHeroChar('A');
-			}
-			
-			// Checks if the player is at the Labyrinth exit
-			if(lab.isAtExit(player)) {
-				
-				// If the dragon is slained our hero may sucessfully exit the Labyrinth 
-				if(!dragon.isAlive())
-					break;
-				else
-					player.MoveBack(lab);
-			}
-
-			
-			// Only draws sword if dragon has not the sword and if it's not taken by the player.
-			if(!dragon.hasSword() && !player.isArmed()) lab.setSword(sword);
-			
-			lab.DrawBoard();
-			
-		}
-		
-		lab.setPlayer(player);
-		
-		lab.DrawBoard();
-		
-		if(player.isAlive()) System.out.print(" WIN ");
-		else System.out.print(" LOSE ");
+		Labyrinth lab = new Labyrinth(9);
 		
 	}
 
--- src/Labyrinth.java
+++ src/Labyrinth.java
@@ -1,13 +1,340 @@
+import java.util.Arrays;
+import java.util.Random;
+import java.util.Stack;
+
 
 public class Labyrinth {
 	
 	// Char matrix representing the labyrinth 
 	private char tiles[][];
+	private int size;
+	private int exit[];
 
 	// Constructor for a Labyrinth type object
-	public Labyrinth(char[][] tiles) {
+	public Labyrinth(int size) {
 		
-		this.tiles = tiles;
+		this.size = size;
+		exit = new int[2];
+		tiles = new char[size][size];
+		
+		SetChamber();
+		DrawGrid();
+		GenerateExit();
+		GenerateWalls();
+		DrawBoard();
+		
+	}
+	
+	/**
+	 * Randomly generates a valid exit for the labyrinth. With the following restrictions:
+	 *   The exit must be on a frontier.
+	 *   The exit must not be on a corner. 
+	 *   
+	 * The coordinates for the generated exit are stored on the exit array. 
+	 */
+	public void GenerateExit() {
+		
+		Random r = new Random();
+		
+		/*
+		 * 0 - Top
+		 * 1 - Right
+		 * 2 - Bottom
+		 * 3 - Left
+		 * 
+		 */
+		int side = r.nextInt(4);
+		
+		boolean done = false;
+		while(!done) {
+			
+			switch(side) {
+			case 0: 
+				exit[0] = 0;
+				// Can't be at (0, 0) nor (0, N).
+				exit[1] = r.nextInt(size - 2) + 1;
+				if(tiles[exit[0] + 1][exit[1]] != 'x') done = true;
+				break;
+			case 1:
+				exit[0] = r.nextInt(size - 2) + 1;
+				exit[1] = size - 1;
+				if(tiles[exit[0]][exit[1] - 1] != 'x') done = true;
+				break;
+			case 2: 
+				exit[0] = size - 1;
+				exit[1] = r.nextInt(size - 2) + 1;
+				if(tiles[exit[0] - 1][exit[1]] != 'x') done = true;
+				break;
+			case 3:
+				exit[0] = r.nextInt(size - 2) + 1;
+				exit[1] = 0;
+				if(tiles[exit[0]][exit[1] + 1] != 'x') done = true;
+				break;
+			}
+			
+		}
+		
+
+		
+	}
+	
+	/**
+	 * Sets the outside wall of the labyrinth.
+	 */
+	public void SetChamber() {
+		
+		// Top wall
+		for(int i = 0; i < size; i++) tiles[0][i] = 'x';
+		
+		// Right wall
+		for(int i = 0; i < size; i++) tiles[i][size-1] = 'x';
+		
+		// Bottom wall
+		for(int i = 0; i < size; i++) tiles[size-1][i] = 'x';
+		
+		// Left wall
+		for(int i = 0; i < size; i++) tiles[i][0] = 'x';
+		
+	}
+	
+	public void DrawGrid() {
+		
+		// Determines whether we are drawing a wall or not.
+		boolean wall = false;
+		// Determines whether we are drawing a line or not.
+		boolean line = false;
+		// Draws grid.
+		for(int i = 1; i < (size - 1); i++) {
+			for(int j = 1; j < (size - 1); j++) {
+				
+				if(wall && (line || j < (size - 2))) tiles[i][j] = 'x';
+				else tiles[i][j] = ' ';
+				
+				if(!line) wall = !wall;
+				
+			}
+			
+			line = !line;
+			
+			if(line) wall = true;
+			else wall = false;
+		}
+		
+		// Test
+		/*
+		for(int i = 0; i < size; i++) {
+			for(int j = 0; j < size; j++) {
+				System.out.print(" " + tiles[i][j] + " ");
+			}
+			System.out.println();
+		}
+		*/
+	}
+	
+	public void GenerateWalls() {
+
+		Random r = new Random();
+
+		// Random direction.
+		/*
+		 * 0 - UP
+		 * 1 - RIGHT
+		 * 2 - DOWN
+		 * 3 - LEFT
+		 */
+		int direction; 
+
+		boolean visited[][] = new boolean[size][size];
+
+		// -- BEGIN Marks all walls and the exit as visited cells, so they don't count. 
+		for(int i = 0; i < size; i++) {
+			for(int j = 0; j < size; j++) {
+				if(tiles[i][j] == 'x' || tiles[i][j] == 'S') visited[i][j] = true;
+				else visited[i][j] = false;
+			}
+		}
+		// -- END 
+
+		// Test visited array.
+		/*
+		for(int i = 0; i < size; i++) {
+			for(int j = 0; j < size; j++) {
+				System.out.print(" " + visited[i][j] + " ");
+			}
+			System.out.println();
+		}
+		 */
+
+		Stack<Integer> stackX = new Stack<Integer>();
+		Stack<Integer> stackY = new Stack<Integer>();
+		//
+		int current[] = new int[2];
+
+		// -- BEGIN Make the initial cell the adjacent to the exit. 
+		current[0] = exit[0]; current[1] = exit[1];
+		if(current[0] == 0) current[0]++;
+		else if(current[0] == (size - 1)) current[0]--;
+		else if(current[1] == 0) current[1]++;
+		else if(current[1] == (size - 1)) current[1]--;
+
+		visited[current[0]][current[1]] = true;
+
+		// Prints current position
+		//System.out.println("Current: " + current[0] + "," + current[1]);
+
+		// -- END 
+
+		boolean done = false;
+		while(!done) {
+
+			// Pick valid neighbor
+			boolean found = false;
+			while(!found) {
+
+				if(!hasUnvisitedNeighbors(current[0], current[1], visited)) {
+
+					if(!stackX.isEmpty()) {
+						current[0] = stackX.pop(); current[1] = stackY.pop();
+					} else {
+						boolean newCell = false;
+						for(int i = 0; i < size; i++) {
+							for(int j = 0; j < size; j++) {
+								if(!visited[i][j]) {
+									current[0] = i; current[1] = j;
+									newCell = true;
+									break;
+								}
+								if(newCell) break;
+							}
+						}
+					}
+
+
+				} else {
+
+					// Push current cell to stack
+					stackX.push(current[0]); stackY.push(current[1]);
+
+					direction = r.nextInt(4);
+
+					if(hasNeighbor(current[0], current[1], direction)) {
+						switch(direction) {
+						// UP
+						case 0:
+							if(!visited[current[0] - 2][current[1]]) {
+								tiles[current[0] - 1][current[1]] = ' ';
+								current[0] -= 2;
+								found = true;
+							}
+							break;
+							// RIGHT
+						case 1:
+							if(!visited[current[0]][current[1] + 2]) {
+								tiles[current[0]][current[1] + 1] = ' ';
+								current[1] += 2;
+								found = true;
+							}
+							break;
+							// DOWN
+						case 2:
+							if(!visited[current[0] + 2][current[1]]) {
+								tiles[current[0] + 1][current[1]] = ' ';
+								current[0] += 2;
+								found = true;
+							}
+							break;
+							// LEFT
+						case 3:
+							if(!visited[current[0]][current[1] - 2]) {
+								tiles[current[0]][current[1] - 1] = ' ';
+								current[1] -= 2;
+								found = true;
+							}
+							break;
+						}
+						visited[current[0]][current[1]] = true;
+					}
+				}
+
+
+
+			}
+
+			// Checks if we still have unvisited cells.
+			for(int i = 0; i < size; i++) {
+				done = true;
+				for(int j = 0; j < size; j++) {
+					if(!visited[i][j]) {
+						done = false;
+						break;
+					}
+				}
+				if(!done) break;
+			}
+
+		}
+
+	}
+	
+	public boolean hasNeighbor(int x, int y, int direction) {
+		
+		switch(direction) {
+		// UP
+		case 0:
+			if((x - 2) < 0) return false;
+			else return true;
+		// RIGHT
+		case 1:
+			if((y + 2) >= size) return false;
+			else return true;
+		// DOWN
+		case 2:
+			if((x + 2) >= size) return false;
+			else return true;
+		// LEFT
+		case 3:
+			if((y - 2) < 0) return false;
+			else return true;
+		}
+		
+		return false;
+		
+	}
+	
+	public boolean hasUnvisitedNeighbors(int x, int y, boolean visited[][]) {
+		
+		boolean result = false;
+		for(int i = 0; i < 4; i++) {
+			switch(i) {
+			// UP
+			case 0:
+				if((x - 2) >= 0) {
+					if(!visited[x - 2][y]) result = true;
+				}
+				break;
+			// RIGHT
+			case 1:
+				if((y + 2) < size) {
+					if(!visited[x][y + 2]) result = true;
+				}
+				break;
+			// DOWN
+			case 2:
+				if((x + 2) < size) {
+					if(!visited[x + 2][y]) result = true;
+				}
+				break;
+			// LEFT
+			case 3:
+				if((y - 2) >= 0) {
+					if(!visited[x][y - 2]) result = true;
+				}
+				break;
+			}
+			
+		}
+		
+		return result;
 	}
 	
 	// Sets the player's position tile with its representing char: 'H' or 'A' (if armed with the sword)
@@ -46,14 +373,14 @@ public class Labyrinth {
 	// Prints the labyrinth tiles (represented by a matrix)
 	public void DrawBoard() {
 		
-		for(char[] line : tiles) {
-			
-			for(char tile : line) {
-				
-				System.out.print(" " + tile + " ");
-				
+		for(int i = 0; i < size; i++) {
+			for(int j = 0; j < size; j++) {
+				if(i == exit[0] && j == exit[1]) {
+					System.out.print(" " + 'S' + " ");
+				} else {
+					System.out.print(" " + tiles[i][j] + " ");
+				}
 			}
-			
 			System.out.println();
 		}
 		
